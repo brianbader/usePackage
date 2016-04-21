57e38a5718d39866b0e9cb5528e284c03be2e12c
bbader <brianbader@mail.com>
2016-04-21 14:30:42 -0400

    first (and hopefully only) commit

---
+++ usePackage.R
@@ -0,0 +1,14 @@
+## Function that checks if a package is installed and if not, installs it
+## Then loads all requested packages using 'require' command
+usePackage <- function(p) {
+  newPackages <- p[!(p %in% installed.packages()[, "Package"])]
+  if(length(newPackages))
+    install.packages(newPackages, dependencies = TRUE)
+  cat("Packages successfully loaded:\n")
+  sapply(p, require, character.only = TRUE)
+}
+
+## Example
+packageList <- c("dplyr", "boot", "random")
+!is.element(packageList, installed.packages()[, "Package"])
+usePackage(packageList)
